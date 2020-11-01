fe803b0892a2b9fbdc867a0d53bd921a10f4e906
Jan Küster <jkuester@uni-bremen.de>
2020-11-01 12:07:47 +0100

    bootstrap 3 package summary updated<

--- bootstrap3/package.js
+++ bootstrap3/package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'mcp:autoform-bootstrap3',
   version: '1.0.0',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Bootstrap 3 theme for aldeed:autoform',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'git@github.com:Meteor-Community-Packages/meteor-autoform-themes.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
