8880ed8ebe6536ed2aa89974e269106c32e504a3
Jan Küster <jkuester@uni-bremen.de>
2021-01-22 14:29:18 +0100

    inputTypes autocomplete update with correct atts

--- bootstrap4/templates/bootstrap4/inputTypes/autocomplete/autocomplete.js
+++ bootstrap4/templates/bootstrap4/inputTypes/autocomplete/autocomplete.js
@@ -1,5 +1,4 @@
 import { Template } from 'meteor/templating'
-import './autocomplete.html'
 
 for (const hook of Template.afAutocomplete._callbacks.rendered) {
   Template.afAutocomplete_bootstrap4.onRendered(hook)
